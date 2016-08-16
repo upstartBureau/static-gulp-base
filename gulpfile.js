17b58910c93ed2932546bc0b57c5b3ab7fe8c70e
mainhoang <mnhoang.80@gmail>
2016-08-15 17:18:19 -0700

    added changes form shotwell working repo

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,20 @@ const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
 const sass = require('gulp-sass')
 
-gulp.task('serve', ['sass', 'html'], function() {
+gulp.task('serve', ['sass', 'move'], function() {
   browserSync.init({
     server: './dist',
     port:1337
   })
-  gulp.watch('src/**', ['html'])
+  gulp.watch('src/**', ['move'])
   gulp.watch('src/sass/*.scss', ['sass'])
   gulp.watch('dist/*.html').on('change', browserSync.reload)
 })
-gulp.task('html', function(){
+gulp.task('move', function(){
   gulp.src('src/html/*.html').pipe(gulp.dest('dist'))
   gulp.src('src/js/*.js').pipe(gulp.dest('dist/js'))
+  gulp.src('src/sass/fonts/**').pipe(gulp.dest('dist/css/fonts'))
+  gulp.src('src/img/**').pipe(gulp.dest('dist/img'))
 })
 gulp.task('sass', function() {
   return gulp.src('src/sass/*.scss')
